3a1cb1f6d90bacc07ffcc199b559f3bcd2ec72d1
larsrossen <lars@hrossen.dk>
2026-01-14 19:07:11 +0100

    added library load option for visual code ssh module to work

--- src/foundation/30-tappaas-cicd/tappaas-cicd.nix
+++ src/foundation/30-tappaas-cicd/tappaas-cicd.nix
@@ -78,6 +78,11 @@
     serviceConfig.Restart = "always"; # restart when session is closed
   };
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
 
   # QEMU Guest Agent
   services.qemuGuest.enable = true;
