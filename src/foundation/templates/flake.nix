3e07ca7d5441dc6bba3b20336b1f46cfcc22022c
larsrossen <lars@hrossen.dk>
2026-09-24 06:59:36 +0200

    chore(pin): estate nixpkgs moves to nixos-26.05

--- src/foundation/templates/flake.nix
+++ src/foundation/templates/flake.nix
@@ -2,7 +2,7 @@
   description = "TAPPaaS prebuilt NixOS VM template image (config baked in)";
 
   # Pinned to the same NixOS release as system.stateVersion in tappaas-common.nix.
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
   outputs = { self, nixpkgs, ... }:
     let
